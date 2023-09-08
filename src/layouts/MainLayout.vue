5621c1ebf70667582604ec2314cbf4c6795cec9d
Juan Pablo <shunikv@gmail.com>
2023-09-08 12:38:13 -0600

    primera carga

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="las la-bars"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <!--TODO: <div>Quasar v{{ $q.version }}</div> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+import { linksList } from "../router/link-list.js";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/router/link-list.js
@@ -0,0 +1,20 @@
+export const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "las la-graduation-cap",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Flex Layout",
+    caption: "Estilos con flex",
+    icon: "las la-layer-group",
+    link: "flex",
+  },
+  {
+    title: "Typography",
+    caption: "Tipo de letras en Quasar",
+    icon: "lab la-vuejs",
+    link: "typography",
+  },
+];
